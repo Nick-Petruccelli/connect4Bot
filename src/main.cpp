0465194db9902ceb961777f23924b611594208df
Nick-Petruccelli <nickpetro18@gmail.com>
2024-10-09 16:34:18 -0400

    corrected bounds checking on player input

--- src/main.cpp
+++ src/main.cpp
@@ -13,24 +13,34 @@ int main() {
 
     int col;
     std::cout << "Player 1 enter your row. (0-6)" << std::endl;
-    std::cin >> col;
-    while (!std::cin) {
-      if (col < 0 || col > BOARDWIDTH) {
+    while (true) {
+      std::cin >> col;
+      if (col < 0 || col >= BOARDWIDTH) {
         std::cout << "You did not enter a valid column try again." << std::endl;
-      } else {
+      } else if (board.getColHeight(col) < 0) {
+        std::cout << "The column you entered is full try again." << std::endl;
+      } else if (!std::cin) {
         std::cout << "You did not enter a number try again." << std::endl;
+      } else {
+        break;
       }
     }
+    std::cout << board.getColHeight(col) << std::endl;
     board.placeToken(col, playerOne);
     playerOne = false;
+    board.printBoard();
 
     std::cout << "Player 2 enter your row. (0-6)" << std::endl;
-    std::cin >> col;
-    while (!std::cin) {
-      if (col < 0 || col > BOARDWIDTH) {
+    while (true) {
+      std::cin >> col;
+      if (col < 0 || col >= BOARDWIDTH) {
         std::cout << "You did not enter a valid column try again." << std::endl;
-      } else {
+      } else if (board.getColHeight(col) < 0) {
+        std::cout << "The column you entered is full try again." << std::endl;
+      } else if (!std::cin) {
         std::cout << "You did not enter a number try again." << std::endl;
+      } else {
+        break;
       }
     }
     board.placeToken(col, playerOne);
